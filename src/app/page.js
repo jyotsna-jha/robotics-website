f723369a9c34e28f255796a12bf00d1330109eed
jyotsna-jha <116620336+jyotsna-jha@users.noreply.github.com>
2023-10-18 21:36:54 +0545

    rac commit1

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+import Herosection from "@/app/components/Herosection";
+import RacImageGallery from "./components/RacImageGallery";
+
+
+const Page = () => {
+    return (
+        <>
+          <Herosection title={"Robotics and Automation Center"} />
+          <RacImageGallery/>
+          
+        </>
+    );
+};
+
+export default Page;
